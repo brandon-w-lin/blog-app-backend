cf653b514bfac196bfae3f4872c180ef1941e819
Brandon Lin <bwlin93@gmail.com>
2022-06-29 16:19:49 -0700

    adds validations for posts: title exists, body exists, view count is int

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
+  validates :title, presence: true, length: { minimum: 2 }
+  validates :body, presence: true, length: { minimum: 5 }
+  validates :views, numericality: { only_integer: true }
 end
